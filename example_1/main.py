b53682eb4050d60d3bf504908c8b5857ddb1784d
baconyao <bacon735392@gmail.com>
2024-12-10 01:30:32 +0800

    Update example 1

--- example_1/main.py
+++ example_1/main.py
@@ -4,14 +4,21 @@ console = logging.StreamHandler()
 file_handler = logging.FileHandler("file.log")
 logging.basicConfig(
     level=logging.DEBUG,
-    format="%(asctime)s %(levelname)s %(name) s:%(lineno)d %(message) s",
+    format="%(levelname)-8s - %(module)-10s: %(funcName)s %(lineno)-4d - "
+    + "%(message)s",
     handlers=[console, file_handler],
 )
 
 logger = logging.getLogger("logging_101_ex1")
 
-logger.debug("Ex1 debug message")
-logger.info("Ex1 informational message")
-logger.warning("Ex1 warning message")
-logger.error("Ex1 error message")
-logger.critical("Ex1 critical message")
+
+def test_func():
+    logger.debug("Ex1 debug message")
+    logger.info("Ex1 informational message")
+    logger.warning("Ex1 warning message")
+    logger.error("Ex1 error message")
+    logger.critical("Ex1 critical message")
+
+
+if __name__ == "__main__":
+    test_func()
