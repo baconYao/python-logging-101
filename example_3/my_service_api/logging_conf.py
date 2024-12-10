522396b0c3ca7ef256b6d7a4d538d8b26562c6b2
baconyao <bacon735392@gmail.com>
2024-12-10 22:56:41 +0800

    Update the logging configuration of example_3

--- example_3/my_service_api/logging_conf.py
+++ example_3/my_service_api/logging_conf.py
@@ -12,18 +12,32 @@ def configure_logging() -> None:
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(levelname)-8s - %(name)s - %(module)-10s:"
                     + " %(funcName)s %(lineno)-4d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",  # noqa: E501,
+                },
             },
             "handlers": {
                 "default": {
                     "class": "logging.StreamHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "my_service_api.log",
+                    "maxBytes": 1024 * 1024,  # 1MB,
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
                 "my_service_api": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "INFO",
                     "propagate": False,
                 },
